09998b191054d098b9dbb16143d06efaeb05d722
erose357 <erose357@gmail.com>
2017-07-13 15:48:21 -0600

    Add delete functionalitygs gs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -41,7 +41,11 @@ class JobsController < ApplicationController
   end
 
   def destroy
-    # implement on your own!
+    job = Job.find(params[:id])
+    job.destroy
+
+    flash[:success] = "#{job.title} was successfully deleted!"
+    redirect_to company_jobs_path
   end
 
   private
---
+++ spec/features/jobs/user_deletes_existing_job_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "User deletes and existing job" do
+  scenario "a user can delete a job" do
+    job = create(:job)
+
+    visit company_job_path(job.company.id, job.id)
+
+    click_link "Delete"
+
+    expect(page).to have_content("Mechanic was successfully deleted!")
+  end
+end
