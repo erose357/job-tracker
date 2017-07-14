d5f4edc296412a58bc54e1b21e6b7a2d192975dd
erose357 <erose357@gmail.com>
2017-07-14 11:33:42 -0600

    Add edit and delete links to index page for categories

--- spec/features/categories/user_views_all_categories_spec.rb
+++ spec/features/categories/user_views_all_categories_spec.rb
@@ -3,9 +3,12 @@ require 'rails_helper'
 describe "User sees all categories" do
   scenario "a user sees all the categories" do
     category = create_list(:category, 5)
-
+# byebug
     visit categories_path
-save_and_open_page
+
     expect(page).to have_content("Category1")
+    # find_link('Edit')[:href].should == "categories/#{category[1].id}"
+    expect(page).to have_link('Edit')#, href: "/categories/#{category[0].id}")
+    expect(page).to have_link('Delete')
   end
 end
