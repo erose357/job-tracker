d87b8d64b09c28da2b859bb4f750d3786914478e
erose357 <erose357@gmail.com>
2017-07-13 19:46:26 -0600

    Add create functionality to category

--- spec/features/categories/user_creates_new_category_spec.rb
+++ spec/features/categories/user_creates_new_category_spec.rb
@@ -2,15 +2,14 @@ require 'rails_helper'
 
 describe "User creates a new category" do
   scenario "a user can create a new category" do
-    category = build(:category)
 
     visit new_category_path
 
-    fill_in "category[title]"
+    fill_in "category[title]", with: "Latex Salesman"
 
-    click_button "Create"
+    click_button "Create Category"
 
-    expect(current_path).to eq("/categories/#{category.id}")
-    expect(page).to have_content(category.title)
+    expect(current_path).to eq("/categories/#{Category.last.id}")
+    expect(page).to have_content(Category.last.title)
     end
 end
