d943510334e42f4349cc61ed3b7c187185f2522a
erose357 <erose357@gmail.com>
2017-07-15 14:51:04 -0600

    Complete categories functionality

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -3,5 +3,12 @@ FactoryGirl.define do
     sequence :title do |i|
       "Marketing#{i}"
     end
+
+    factory :category_with_jobs do
+      after(:create) do |category|
+        create_list(:job, 2, :category => category)
+      end
+    end
+
   end
 end
---
+++ spec/features/categories/user_views_one_category_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "User views one category" do
+  scenario "User sees the category and associated jobs" do
+    category = create(:category_with_jobs)
+
+    visit category_path(category)
+
+    expect(current_path).to eq("/categories/#{category.id}")
+    expect(page).to have_content("#{category.title}")
+    expect(page).to have_content("#{category.jobs[0].title}")
+    expect(page).to have_content("#{category.jobs[1].title}")
+  end
+end
